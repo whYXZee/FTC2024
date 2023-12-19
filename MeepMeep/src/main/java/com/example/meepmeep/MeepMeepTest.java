58a4c901af9f5e0a26779d7fa90c6badf113da7a
Vedant Mohapatra <vedant.mohapatra@gmail.com>
2023-12-19 17:37:02 -0600

    Added trajectories to auton

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTest.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTest.java
@@ -14,19 +14,8 @@ public class MeepMeepTest {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(30, 10, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-36.74, 66.05, Math.toRadians(270.00)))
-                                .splineTo(new Vector2d(-36.06, 50.94), Math.toRadians(-87.40))
-                                .splineTo(new Vector2d(-35.83, 34.45), Math.toRadians(6.01))
-                                .splineTo(new Vector2d(-28.27, 32.85), Math.toRadians(-11.98))
-                                .splineTo(new Vector2d(-37.20, 32.62), Math.toRadians(-80.84))
-                                .splineTo(new Vector2d(-34.68, 17.51), Math.toRadians(-80.54))
-                                .splineTo(new Vector2d(-35.37, 8.36), Math.toRadians(265.71))
-                                .splineTo(new Vector2d(-23.24, 10.19), Math.toRadians(8.58))
-                                .splineTo(new Vector2d(-5.61, 10.19), Math.toRadians(0.00))
-                                .splineTo(new Vector2d(12.02, 10.19), Math.toRadians(0.00))
-                                .splineTo(new Vector2d(48.65, 11.33), Math.toRadians(1.79))
-                                .splineTo(new Vector2d(42.70, 36.74), Math.toRadians(95.34))
-                                .splineTo(new Vector2d(49.11, 36.06), Math.toRadians(3.81))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35.00, 61.00, Math.toRadians(-90.00)))
+                                .lineToSplineHeading(new Pose2d(-35.00, 34.45, Math.toRadians(90.00)))
                                 .build()
 
                 );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorThresholdAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorThresholdAuton.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -11,62 +15,42 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 @Autonomous
 
-public class ColorThresholdAuton extends OpenCvPipeline {
-    private String location = "nothing"; // output
-    public Scalar lower = new Scalar(0, 0, 0); // HSV threshold bounds
-    public Scalar upper = new Scalar(255, 255, 255);
+public class ColorThresholdAuton extends LinearOpMode {
+    private int position;
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-    private Mat hsvMat = new Mat(); // converted image
-    private Mat binaryMat = new Mat(); // imamge analyzed after thresholding
-    private Mat maskedInputMat = new Mat();
+        waitForStart();
 
-    // Rectangle regions to be scanned
-    private Point topLeft1 = new Point(10, 0), bottomRight1 = new Point(40, 20);
-    private Point topLeft2 = new Point(10, 0), bottomRight2 = new Point(40, 20);
+        if(isStopRequested()) return;
 
-    public ColorThresholdAuton() {
+        // color thresholding code here
+        // return int of prop position
 
-    }
+        // if position 1
+        if (position == 1) {
+            Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d(-35.00, 60.00, Math.toRadians(-90.00)))
+                    .lineToSplineHeading(new Pose2d(-35.00, 34.45, Math.toRadians(180.00)))
+                    .build();
 
-    @Override
-        public Mat processFrame (Mat input){
-            // Convert from BGR to HSV
-            Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
-            Core.inRange(hsvMat, lower, upper, binaryMat);
+            drive.followTrajectory(myTrajectory);
+        }
+        // if position 3
+         else if (position == 3) {
+            Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d(-35.00, 60.00, Math.toRadians(-90.00)))
+                    .lineToSplineHeading(new Pose2d(-35.00, 34.45, Math.toRadians(0.00)))
+                    .build();
 
+            drive.followTrajectory(myTrajectory);
+        } else {
+            Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d(-35.00, 60.00, Math.toRadians(-90.00)))
+                    .lineToSplineHeading(new Pose2d(-35.00, 34.45, Math.toRadians(90.00)))
+                    .build();
 
-            // Scan both rectangle regions, keeping track of how many
-            // pixels meet the threshold value, indicated by the color white
-            // in the binary image
-            double w1 = 0, w2 = 0;
-            // process the pixel value for each rectangle  (255 = W, 0 = B)
-            for (int i = (int) topLeft1.x; i <= bottomRight1.x; i++) {
-                for (int j = (int) topLeft1.y; j <= bottomRight1.y; j++) {
-                    if (binaryMat.get(i, j)[0] == 255) {
-                        w1++;
-                    }
-                }
-            }
-
-            for (int i = (int) topLeft2.x; i <= bottomRight2.x; i++) {
-                for (int j = (int) topLeft2.y; j <= bottomRight2.y; j++) {
-                    if (binaryMat.get(i, j)[0] == 255) {
-                        w2++;
-                    }
-                }
-            }
-
-            // Determine object location
-            if (w1 > w2) {
-                location = "1";
-            } else if (w1 < w2) {
-                location = "2";
-            }
-
-            return binaryMat;
+            drive.followTrajectory(myTrajectory);
         }
 
-        public String getLocation () {
-            return location;
-        }
+
+
     }
+}
